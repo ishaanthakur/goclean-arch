a4ef0c6c5f39c31495f22a66b3e5a5e5a8146927
Ishaan Thakur <ishaan.thakur57@gmail.com>
2020-01-27 11:51:05 -0500

    made extra changes

--- author/repository.go
+++ author/repository.go
@@ -3,7 +3,7 @@ package author
 import (
 	"context"
 
-	"github.com/bxcodec/go-clean-arch/models"
+	"github.com/ishaanthakur/goclean-arch/models"
 )
 
 // Repository represent the author's repository contract
